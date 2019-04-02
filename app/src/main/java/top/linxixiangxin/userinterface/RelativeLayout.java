defb84d0c49c1e61ca0e2eb3cd9004f6bea0c936
liusu0325 <908148781@qq.com>
2019-04-02 09:29:20 +0800

    Initial commit

---
+++ app/src/main/java/top/linxixiangxin/userinterface/RelativeLayout.java
@@ -0,0 +1,13 @@
+package top.linxixiangxin.userinterface;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class RelativeLayout extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_relative_layout);
+    }
+}
---
+++ app/src/main/java/top/linxixiangxin/userinterface/RelativeLayout2.java
@@ -0,0 +1,13 @@
+package top.linxixiangxin.userinterface;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class RelativeLayout2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_relative_layout2);
+    }
+}
