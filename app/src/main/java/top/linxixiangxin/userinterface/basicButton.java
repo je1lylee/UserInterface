e0512c20e6bb6871ceb107093ad601f93cc10229
liusu0325 <908148781@qq.com>
2019-04-14 18:00:49 +0800

    s

--- app/src/main/java/top/linxixiangxin/userinterface/basicButton.java
+++ app/src/main/java/top/linxixiangxin/userinterface/basicButton.java
@@ -2,6 +2,8 @@ package top.linxixiangxin.userinterface;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 public class basicButton extends AppCompatActivity {
 
@@ -9,5 +11,9 @@ public class basicButton extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_basic_button);
+    }
+
+    public void Buttoner(View view) {
+        Toast.makeText(this, "按钮被按下", Toast.LENGTH_SHORT).show();
     }
 }
