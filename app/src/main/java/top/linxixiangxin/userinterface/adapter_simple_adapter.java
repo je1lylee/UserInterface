ed7a407f2743585caff31b996805bee87db5a28f
liusu0325 <908148781@qq.com>
2019-04-27 23:20:50 +0800

    添加注释

--- app/src/main/java/top/linxixiangxin/userinterface/adapter_simple_adapter.java
+++ app/src/main/java/top/linxixiangxin/userinterface/adapter_simple_adapter.java
@@ -2,6 +2,7 @@ package top.linxixiangxin.userinterface;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -20,16 +21,18 @@ public class adapter_simple_adapter extends AppCompatActivity implements Adapter
     private ListView listView;
     private SimpleAdapter simpleAdapter;
     private List<HashMap<String,Object>> dataList;
+    private static final String TAG = "dialog";
     /**
      * 组织数据
      */
     private void initData(){
+        //初始化数据，创建一个长度为10的列表
         dataList = new ArrayList<HashMap<String, Object>>();
         for (int i = 0;i<10;i++){
             HashMap<String,Object> hashMap = new HashMap<String,Object>();
             hashMap.put("image",R.drawable.apple_pic);
             hashMap.put("info","第"+i+"个苹果");
-            hashMap.put("cbx",(i<5? true:false));
+            hashMap.put("cbx",(i<5? true:false));//设置前五项为选中状态
             dataList.add(hashMap);
         }
     }
@@ -38,18 +41,18 @@ public class adapter_simple_adapter extends AppCompatActivity implements Adapter
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_adapter_simple_adapter);
 
-        initData();
+        initData();//初始化数据
         listView = findViewById(R.id.lv_simpleadapter);
-        simpleAdapter = new SimpleAdapter(
-                this,
-                dataList,
-                R.layout.simple_item,
-                new String[]{"image","info","cbx"},
-                new int[]{R.id.img_simple,R.id.tv_content,R.id.cbx}){
+        simpleAdapter = new SimpleAdapter(//创建SimpleAdapter
+                this,//上下文
+                dataList,//数据源
+                R.layout.simple_item,//布局
+                new String[]{"image","info","cbx"},//布局中的控件名
+                new int[]{R.id.img_simple,R.id.tv_content,R.id.cbx}){//寻找控件ID
             @Override
             public View getView(final int position, View convertView, ViewGroup parent) {
-                View view = LayoutInflater.from(getApplicationContext()).inflate(R.layout.simple_item,parent,false);
-                TextView textView = view.findViewById(R.id.tv_content);
+                View view = LayoutInflater.from(getApplicationContext()).inflate(R.layout.simple_item,parent,false);//获得创建在页面中的自定义VIEW
+                TextView textView = view.findViewById(R.id.tv_content);//寻找控件 并设置控件的内容和初始状态。
                 CheckBox checkBox = view.findViewById(R.id.cbx);
                 textView.setText(dataList.get(position).get("info").toString());
                 checkBox.setChecked((Boolean) dataList.get(position).get("cbx"));
@@ -62,14 +65,14 @@ public class adapter_simple_adapter extends AppCompatActivity implements Adapter
                 return view;
             }
         };
-        listView.setAdapter(simpleAdapter);
-        listView.setOnItemClickListener(this);
+        listView.setAdapter(simpleAdapter);//绑定适配器
+        listView.setOnItemClickListener(this);//绑定监听
     }
 
     @Override
     public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-        dataList.remove(position);
-        simpleAdapter.notifyDataSetChanged();
+        dataList.remove(position);//点谁谁从datalist中消失
+        simpleAdapter.notifyDataSetChanged();//读datalist刷新view
     }
 
     /**
@@ -79,10 +82,12 @@ public class adapter_simple_adapter extends AppCompatActivity implements Adapter
     public void onDeleteAllSelected(View view){
         for(int i=0;i<dataList.size();i++){
             HashMap<String,Object> hashMap = dataList.get(i);
+            Log.d(TAG, "OUT I"+i);
             if ((Boolean) hashMap.get("cbx")){
                 dataList.remove(i--);
+                Log.d(TAG, "I.VALUE: "+i);
             }
         }
-        simpleAdapter.notifyDataSetChanged();
+        simpleAdapter.notifyDataSetChanged();//刷新view
     }
 }
